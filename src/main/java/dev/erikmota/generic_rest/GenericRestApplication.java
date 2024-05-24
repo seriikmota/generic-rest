a072e3ec20a847afc48813896299c48828a3ad60
Erik Mota <seriikmota.dev@gmail.com>
2024-05-24 11:43:03 -0300

    first commit

---
+++ src/main/java/dev/erikmota/generic_rest/GenericRestApplication.java
@@ -0,0 +1,13 @@
+package dev.erikmota.generic_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/erikmota/generic_rest/GenericRestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.erikmota.generic_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
